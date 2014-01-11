380a84a9eec47e0c6deeec2e428ed828ec803b47
Milan Patel <patelmilan.p@gmail.com>
2014-01-11 21:58:05 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClassalyzeDemo::Application.config.secret_key_base = '49394001fa5fbcb791587c650c3fa54921f98f8d60f9960c07945a2c2683ff50964753fb69a320a5a2c3d5c4b6d880a913d6d89028aa33f6bb26d242ed4c8f9b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ClassalyzeDemo::Application.config.session_store :cookie_store, key: '_classalyze-demo_session'
