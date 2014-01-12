aa41d9b092588247b4bd2544de1c0db10092dbc6
Milan Patel <patelmilan.p@gmail.com>
2014-01-13 02:07:26 +0530

    fixed issue with turbolinks

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,51 +1,54 @@
-$(document).ready(function(){
+var createChart = function() {
   $.ajax({
-      url: "http://localhost:3000/classrooms/7",
-      type: "GET",
-      dataType : "json",
+    url: "http://localhost:3000/classrooms/7",
+    type: "GET",
+    dataType : "json",
 
-      success: function( json ) {
+    success: function( json ) {
 
-          initial = [{x:0, y:0}];
-          result = [{ values: initial.concat(json.student7),
-                      key: 'Student 7',
-                      color: '#ff7f0e' },
-                    { values: initial.concat(json.classroom),
-                      key: 'Classroom 9A',
-                      color: '#2ca02c' }];
+        initial = [{x:0, y:0}];
+        result = [{ values: initial.concat(json.student7),
+                    key: 'Student 7',
+                    color: '#ff7f0e' },
+                  { values: initial.concat(json.classroom),
+                    key: 'Classroom 9A',
+                    color: '#2ca02c' }];
 
-          // create line chart
-          nv.addGraph(function() {
-            var chart = nv.models.lineChart()
-                        .options({
-                          x: function(d,i) { return i},
-                        });
+        // create line chart
+        nv.addGraph(function() {
+          var chart = nv.models.lineChart()
+                      .options({
+                        x: function(d,i) { return i},
+                      });
 
-            var x = d3.scale.ordinal().domain(["0", "FA1","FA2", "SA1"]).rangePoints([1, 1060]);
-            chart.xAxis
-                .axisLabel('Tests')
-                .scale(x)
+          var x = d3.scale.ordinal().domain(["0", "FA1","FA2", "SA1"]).rangePoints([1, 1060]);
+          chart.xAxis
+              .axisLabel('Tests')
+              .scale(x)
 
-            chart.yAxis
-                .axisLabel('Scores')
-                .tickFormat(function(d, i){ return d + "%" });
+          chart.yAxis
+              .axisLabel('Scores')
+              .tickFormat(function(d, i){ return d + "%" });
 
-            d3.select('#chart svg')
-                .datum(result)
-                .transition().duration(500)
-                .call(chart);
+          d3.select('#chart svg')
+              .datum(result)
+              .transition().duration(500)
+              .call(chart);
 
-            return chart;
-          });
-      },
+          return chart;
+        });
+    },
 
-      error: function( xhr, status ) {
-          alert( "Sorry, there was a problem!" );
-      },
+    error: function( xhr, status ) {
+        alert( "Sorry, there was a problem!" );
+    },
 
-      // code to run regardless of success or failure
-      //complete: function( xhr, status ) {
-          //alert( "The request is complete!" );
-      //}
+    // code to run regardless of success or failure
+    //complete: function( xhr, status ) {
+        //alert( "The request is complete!" );
+    //}
   });
-});
+};
+
+$(document).ready(createChart);
+$(document).on('page:load', createChart);
